f98791f4ed892ecc031ec357928abfea2aeacf6b
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-03-30 23:12:53 +0530

    updated blog routes

--- backend/src/blog.ts
+++ backend/src/blog.ts
@@ -7,7 +7,8 @@ export const blog = new Hono<{
         JWT_SECRET:string
     },
     Variables:{
-        userId : string
+        userId : string,
+        prisma: any
     }
 }>();
 
@@ -28,20 +29,88 @@ export const blog = new Hono<{
     }
   })
   
-  blog.post('/', (c) => {
-    return c.text('Hello Hono!')
+  blog.post('/', async (c) => {
+    const userId = c.get('userId')
+    const prisma = c.get('prisma')
+    const body = await c.req.json();
+
+    try{
+      await prisma.Post.create({
+        data:{
+            title: body.title,
+            content: body.content,
+            authorId: userId
+        }
+      })
+
+      return c.json({
+        msg : "Blog created successfully"
+      })
+    }catch(e){
+      console.log(e)
+      return c.json({
+        msg: "error while adding blog"
+      })
+    }
+    
   })
   
-  blog.put('/', (c) => {
-    return c.text('Hello Hono!')
+  blog.put('/', async (c) => {
+    const prisma = c.get('prisma')
+    const body = await c.req.json();
+    const post_id = body.postId;
+    const updates:any = {}
+
+    if(body.title) updates.title = body.title
+    if(body.content) updates.content = body.content
+
+    try{
+      await prisma.Post.update({
+        where:{
+          id : post_id
+        },
+        data : updates
+      })
+
+      return c.json({
+        msg : "Successfully updated the post"
+      })
+    }
+    catch(e){
+      console.log(e)
+      return c.json({
+        msg : "Error while updating the post"
+      })
+    }
   })
   
-  blog.get('/:id', (c) => {
-    const id = c.req.param;
-  
-    return c.text('Hello Hono!')
+  blog.get('/:id', async (c) => {
+    const id = c.req.param('id');
+    console.log(id)
+    const prisma = c.get('prisma')
+
+    try{
+      const post = await prisma.Post.findFirst({
+        where:{
+          id
+        }
+      })
+      return c.json({
+        post
+      })
+    }
+    catch(e){
+      return c.json({
+        msg:"Error while fetching the post"
+      })
+    }
   })
   
-  blog.get('/bulk', (c) => {
-    return c.text('Hello Hono!')
+  blog.get('/post/bulk', async (c) => {
+      const prisma = c.get('prisma')
+      const posts = await prisma.Post.findMany()
+
+      return c.json({
+        posts
+      })
   })
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -19,9 +19,7 @@ app.use('*', async (c,next) => {
 	const prisma = new PrismaClient({
       datasourceUrl: c.env.DATABASE_URL,
   }).$extends(withAccelerate());
-
   c.set('prisma', prisma);
-
   await next();
 })
 
