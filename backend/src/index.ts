3163f4cbc1bd13c9c5cbb257176d7f44b88adb7f
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-03-30 19:28:41 +0530

    some routes have been setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,14 +1,99 @@
 import { Hono } from 'hono'
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+import {sign,decode,verify} from 'hono/jwt'
 
-const app = new Hono()
+const app = new Hono<{
+  Bindings : {
+    DATABASE_URL : string,
+    JWT_SECRET: string
+  },
+  Variables: {
+    userId : string,
+    prisma : any
+  }
+}>()
 
-app.post('/api/v1/user/signup', (c) => {
-  
-  return c.text('Hello Hono!')
+app.use('*', async (c,next) => {
+	const prisma = new PrismaClient({
+      datasourceUrl: c.env.DATABASE_URL,
+  }).$extends(withAccelerate());
+
+  c.set('prisma', prisma);
+
+  await next();
 })
 
-app.post('/api/v1/user/signin', (c) => {
-  return c.text('Hello Hono!')
+app.use('/api/v1/blog/*', async(c,next)=>{
+  const token_header = c.req.header('authorization') || "no user";
+  const token = token_header.split(' ')[1];
+
+  try{
+    const user = await verify(token,c.env.JWT_SECRET);
+    c.set("userId",user.id) // later get it by c.get
+    await next();
+  }
+  catch(e){
+    c.status(403)
+    return c.json({
+      msg:"User is not authenticated",
+    })
+  }
+  
+})
+
+app.post('/api/v1/user/signup', async (c) => {
+  const prisma = c.get('prisma')
+
+  const body = await c.req.json();
+
+  try{
+    const user = await prisma.user.create({
+      data:{
+        name: body.name,
+        email: body.email,
+        password: body.password
+      }
+    })
+
+    const token = await sign({id :user.id},c.env.JWT_SECRET)
+      
+    return c.json({
+      token
+    })
+
+  }
+  catch(e){
+    return c.json({
+      msg : "Error while signing up"
+    })
+  }
+
+  
+})
+
+app.post('/api/v1/user/signin', async (c) => {
+  const prisma = c.get('prisma')
+
+  const body = await c.req.json();
+
+  try{
+    const user:any = await prisma.user.findFirst({
+      where:{
+        name : body.name,
+        password: body.password
+      }
+    })
+    const token = await sign({id:user.id},c.env.JWT_SECRET)
+    return c.json({
+      token
+    })
+  }
+  catch(e){
+    return c.json({
+      msg:"Error while signing in"
+    })
+  }
 })
 
 app.post('/api/v1/blog', (c) => {
