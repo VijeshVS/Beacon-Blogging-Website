a824191c3d027a7b1a2f85b73e06514b44c44185
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-04-02 00:34:03 +0530

    Working on progress

---
+++ frontend/src/components/MyFeedBlog.jsx
@@ -0,0 +1,9 @@
+import { BlogComponent } from "./BlogComponent";
+
+export function MyFeedBlog(){
+    return <div className="pl-12 pt-6 h-screen flex">
+        <div className="pr-10">
+        <BlogComponent/> 
+        </div>
+    </div>
+} 
--- frontend/src/pages/Feed.jsx
+++ frontend/src/pages/Feed.jsx
@@ -1,9 +1,21 @@
 import {CreateBlogPostNavbar} from '../components/CreateBlogNavBar'
 import {useNavigationHandler} from '../hooks/useNavigationHandler'
+import {MyFeedBlog} from '../components/MyFeedBlog'
+import { useState } from 'react';
 
 export function Feed (){
+    const [navActive,setnavActive] = useState(true);
+    let navStyling = 'text-lg ml-3 hover:border-b-2 hover:border-gray-300 pb-1 cursor-pointer';
+    let foryouStyling = navActive?navStyling+' text-blue-600':navStyling
+    let myblogStyling = navActive?navStyling:navStyling+' text-blue-600'
+
     const navigator = useNavigationHandler();
     return <div>
         <CreateBlogPostNavbar text="New Blog" toRoute="newblog" handler = {navigator}/>
+        <div className='flex px-10 mt-4'>
+        <h1 className={foryouStyling} onClick={()=>setnavActive(true)}>For you</h1>
+        <h1 className={myblogStyling} onClick={()=>setnavActive(false)}>My Blogs</h1>
+        </div>
+        <MyFeedBlog/>
     </div>    
 }
