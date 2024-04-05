2391b71626696cee1eabc83fa2b3d7898ecafe3e
Vijesh Shetty <vijeshsshetty@gmail.com>
2024-04-05 14:39:55 +0530

    pic bug fixed

--- frontend/src/components/AuthorDetails.jsx
+++ frontend/src/components/AuthorDetails.jsx
@@ -3,7 +3,7 @@ export function AuthorDetails({author}){
     return <div className="m-20">
         <h1 className="text-gray-500 ml-1 text-xl">Author</h1>
         <div className="flex mt-6">
-            <img src="https://avatar.iran.liara.run/public/30" className="h-16 w-16 rounded-2xl"/>
+            <img src={author.imgLink} className="h-16 w-16 rounded-2xl"/>
             <div className="flex ml-4 items-center">
             <h1 className="font-medium">{author.name}</h1>
             </div>
